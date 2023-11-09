c5d7bc3e1e0703adffaa3ad78bd276fa12fc93b9
Franklin Diaz <fdiaz@adaptivecomputing.com>
2023-11-08 22:17:07 -0500

    images until level 8

--- lib/models/flip_card_model.dart
+++ lib/models/flip_card_model.dart
@@ -26,20 +26,29 @@ class DummyData {
     MetaGameController metaCardGameController = Get.find<MetaGameController>();
 
     int numberCard = metaCardGameController.level.value > 1 ? 6 : 3;
-    List pathImg = pathImgLevel1_3;
+    List pathImg = pathImgLevel_1;
 
     // set path image by level
-    if (metaCardGameController.level.value > 3) {
-      pathImg = pathImgLevel3_6;
+    if (metaCardGameController.level.value == 2) {
+      pathImg = pathImgLevel_2;
     }
-    if (metaCardGameController.level.value > 6) {
-      pathImg = pathImgLevel6_9;
+    if (metaCardGameController.level.value == 3) {
+      pathImg = pathImgLevel_3;
     }
-    if (metaCardGameController.level.value > 9) {
-      pathImg = pathImgLevel_9_15;
+    if (metaCardGameController.level.value == 4) {
+      pathImg = pathImgLevel_4;
     }
-    if (metaCardGameController.level.value > 15) {
-      pathImg = pathImgLevel_15_20;
+    if (metaCardGameController.level.value == 5) {
+      pathImg = pathImgLevel_5;
+    }
+    if (metaCardGameController.level.value == 6) {
+      pathImg = pathImgLevel_6;
+    }
+    if (metaCardGameController.level.value == 7) {
+      pathImg = pathImgLevel_7;
+    }
+    if (metaCardGameController.level.value >= 8) {
+      pathImg = pathImgLevel_8;
     }
 
     pathImg.shuffle();
@@ -64,53 +73,74 @@ class DummyData {
     }
   }
 
-  //variades
-  List<String> pathImgLevel1_3 = [
-    "assets/image/flip_card/cat.png",
-    "assets/image/flip_card/elephant.png",
-    "assets/image/flip_card/mouse.png",
-    "assets/image/flip_card/stick.png",
-    "assets/image/flip_card/fish.png",
-    "assets/image/flip_card/leopard.png",
+  List<String> pathImgLevel_1 = [
+    "assets/image/level1/image1.jpg",
+    "assets/image/level1/image2.png",
+    "assets/image/level1/image3.png",
+    "assets/image/level1/image4.png",
+    "assets/image/level1/image5.png",
   ];
 
-  //animales
-  List<String> pathImgLevel3_6 = [
-    "assets/image/flip_card/cat.png",
-    "assets/image/flip_card/elephant.png",
-    "assets/image/flip_card/mouse.png",
-    "assets/image/flip_card/stick.png",
-    "assets/image/flip_card/fish.png",
-    "assets/image/flip_card/leopard.png",
+  List<String> pathImgLevel_2 = [
+    "assets/image/level2/image1.png",
+    "assets/image/level2/image2.png",
+    "assets/image/level2/image3.png",
+    "assets/image/level2/image4.png",
+    "assets/image/level2/image5.png",
+    "assets/image/level2/image6.png",
   ];
 
-  //frutis
-  List<String> pathImgLevel6_9 = [
-    "assets/image/flip_card/cat.png",
-    "assets/image/flip_card/elephant.png",
-    "assets/image/flip_card/mouse.png",
-    "assets/image/flip_card/stick.png",
-    "assets/image/flip_card/fish.png",
-    "assets/image/flip_card/leopard.png",
+  List<String> pathImgLevel_3 = [
+    "assets/image/level3/image1.png",
+    "assets/image/level3/image2.png",
+    "assets/image/level3/image3.png",
+    "assets/image/level3/image4.png",
+    "assets/image/level3/image5.png",
+    "assets/image/level3/image6.png",
   ];
 
-  //cards
-  List<String> pathImgLevel_9_15 = [
-    "assets/image/flip_card/cat.png",
-    "assets/image/flip_card/elephant.png",
-    "assets/image/flip_card/mouse.png",
-    "assets/image/flip_card/stick.png",
-    "assets/image/flip_card/fish.png",
-    "assets/image/flip_card/leopard.png",
+  List<String> pathImgLevel_4 = [
+    "assets/image/level4/image1.png",
+    "assets/image/level4/image2.png",
+    "assets/image/level4/image3.png",
+    "assets/image/level4/image4.png",
+    "assets/image/level4/image5.png",
+    "assets/image/level4/image6.png",
   ];
 
-  //faces
-  List<String> pathImgLevel_15_20 = [
-    "assets/image/flip_card/cat.png",
-    "assets/image/flip_card/elephant.png",
-    "assets/image/flip_card/mouse.png",
-    "assets/image/flip_card/stick.png",
-    "assets/image/flip_card/fish.png",
-    "assets/image/flip_card/leopard.png",
+  List<String> pathImgLevel_5 = [
+    "assets/image/level5/image1.png",
+    "assets/image/level5/image2.png",
+    "assets/image/level5/image3.png",
+    "assets/image/level5/image4.png",
+    "assets/image/level5/image5.png",
+    "assets/image/level5/image6.png",
+  ];
+
+  List<String> pathImgLevel_6 = [
+    "assets/image/level6/image1.png",
+    "assets/image/level6/image2.png",
+    "assets/image/level6/image3.png",
+    "assets/image/level6/image4.png",
+    "assets/image/level6/image5.png",
+    "assets/image/level6/image6.png",
+  ];
+
+  List<String> pathImgLevel_7 = [
+    "assets/image/level7/image1.png",
+    "assets/image/level7/image2.png",
+    "assets/image/level7/image3.png",
+    "assets/image/level7/image4.png",
+    "assets/image/level7/image5.png",
+    "assets/image/level7/image6.png",
+  ];
+
+  List<String> pathImgLevel_8 = [
+    "assets/image/level8/image1.png",
+    "assets/image/level8/image2.png",
+    "assets/image/level8/image3.png",
+    "assets/image/level8/image4.png",
+    "assets/image/level8/image5.png",
+    "assets/image/level8/image6.png",
   ];
 }
